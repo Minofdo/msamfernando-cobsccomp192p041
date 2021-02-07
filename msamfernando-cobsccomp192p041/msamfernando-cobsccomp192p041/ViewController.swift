977795dfd3c55d158422bfe0c49694649c375d24
Minofdo <44726915+Minofdo@users.noreply.github.com>
2021-02-07 16:36:18 +0530

    WinWinCalculator
    M S A M Fernando
    Cobsccomp192p-041
    BSc(Hons) Computing 19.2

---
+++ msamfernando-cobsccomp192p041/msamfernando-cobsccomp192p041/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  msamfernando-cobsccomp192p041
+//
+//  Created by Mino Fdo on 2021-02-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
